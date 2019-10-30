cc88fe597a33233722ceab32669646f738a65755
m-ling <2399144697@.com>
2019-10-30 17:11:34 +0800

    the_4_19/10/30

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,18 +1,40 @@
 <template>
     <div id="login">
       <div v-show="!islogin" class="shade"></div>
-       <div class="log-swp">
+       <div class="log-swp" :class="{down:islogin}">
         <div class="log-logo">
-            <p class="iconfont icon-cha-copy"></p>
+            <p class="iconfont icon-cha-copy" @click="LogDel"></p>
             <div>
                 <img src="@/assets/img/logo-fmo-white.svg" alt="">
+                <h4>fans-of-m.o.</h4>
+                <span>a-new-way-of-recognizing-our-valued-guests</span>
             </div>
+           
         </div>
-        <swiper :options="swiperOption" ref="mySwiperL" id="swipt-L" :class="{down:islogin}">
+        <swiper :options="swiperOption" ref="mySwiperL" id="swipt-L" >
           <!-- slides -->
           <swiper-slide>
           <div  class="log-login">
-
+            <div class="log-left">
+              <h4>im-a-fan</h4>
+              <g-input :title="'会员名或邮箱'" :inform="'请输入会员名或邮箱'"></g-input>
+               <g-input :title="'密码'" :inform="'请输入密码'"></g-input>
+               <a href="">忘记密码？</a>
+               <button>登&nbsp;&nbsp;&nbsp;&nbsp;录</button>
+            </div>
+            <div class="log-right">
+              <h4>become-a-fan-of-mo</h4>
+               <p>join-fans-of-mo-for-faster-booking-complimentary-wifi-and-exclusive</p> 
+               <ul>
+                  <li>-daily-breakfast</li>
+                  <li>-dining-or-spa-credit</li> 
+                  <li>-room-upgrade</li> 
+                  <li>-early-check-in</li> 
+                  <li>-late-check-out</li> 
+                  <li>-streaming-wifi</li> 
+                  <li>-celebratory-treat</li> 
+                  <li>-pressing-services</li></ul>
+                  <a href="/fans-of-mo">learn-more</a></div>
           </div>
         </swiper-slide>
            <swiper-slide>
@@ -28,6 +50,8 @@
     </div>
 </template>
 <script>
+import gInput from "./mini-component/g-input.vue"
+
   export default {
     name: 'carrousel',
     data() {
@@ -42,16 +66,26 @@
         }
       }
     },
+    components:{ gInput },
+    methods:{
+       LogDel(){
+      console.log(123);
+      this.islogin=true;
+    },
+    },
     computed: {
       swiper() {
         return this.$refs.mySwiperL.swiper
       }
     },
+   
     mounted() {
       // current swiper instance
       // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
       console.log('this is current swiper instance object', this.swiper)
-      this.swiper.slideTo(3, 1000, false)
+      // this.swiper.slideTo(3, 1000, false)
+      // 
+      
     }
   }
 </script>
@@ -96,6 +130,7 @@
     background: #413838;
     color: white;
     box-shadow: 0 2px 24px 3px rgba(0,0,0,0.25);
+    transition: .5s;
 }
 .log-logo .icon-cha-copy{
     color: #fffffdc4;
@@ -106,5 +141,49 @@
 }
 .log-logo .icon-cha-copy:hover{
      color: #ffffff;
+}
+.log-left{
+    width: 32%;
+    background: chartreuse;
+    border-right: 2px solid #000;
+    padding: 19px 68px;
+}
+.log-left a{
+ color:cornflowerblue;
+ display: block;
+}
+.log-left button{
+    width: 76%;
+    height: 46px;
+    background: crimson;
+    color: #ffffff;
+    border: 0;
+    border-radius: 3px;
+}
+.log-right{
+  width: 32%;
+  background:#fa0650;
+}
+.log-right li{
+    margin-left: 5px;
+    width: 37%;
+    float: left;
+    line-height: 30px;
+    font-family: 'Trade Gothic LT W04 Bd CnNo-20','ArialNarrow','Arial-Narrow','Arial Narrow',Arial,sans-serif;
+    text-transform: uppercase;
+}
+.log-login{
+  display: flex;
+}
+.log-right a{
+  clear: both;
+}
+.log-right ul{
+  width: 100%;
+}
+.log-right h4{
+  font-size: 1.375rem;  
+  font-family: "Didot LT W02 Roman",Didot,"Hoefler Text",Garamond,"Times New Roman",serif;
+  text-transform: none;
 }
 </style>
---
+++ src/components/mini-component/g-input.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="g-inp">
+    <label for="in">
+      <p>{{title}}</p>
+     <div class="inpBlock">
+        <input type="text" name="" id="in" :placeholder="inform">
+     </div>
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  props:["title",'inform'],
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style scoped>
+#g-inp{
+  width: 50%;
+
+}
+.inpBlock{
+    height: 45px;
+    width: 343px;
+    background: #fbfaf8;
+    text-align: center;
+    line-height: 45px;
+    border: 1px solid #cabfbf;
+    border-radius: 2px;
+    position: relative;
+
+
+}
+.inpBlock #in{
+ height: 90%;
+ width: 90%;
+ outline: 0;
+ border: 0;
+}
+.inpBlock::before{
+    content: '';
+    display: block;
+    width: 0;
+    height: 0;
+    border: inset 7px;
+    border-color: transparent #98711a transparent transparent;
+    border-right-style: solid;
+    border-left-width: 0;
+    position: absolute;
+    top: -2px;
+    left: 1px;
+    -webkit-transform: rotate(45deg);
+    transform: rotate(45deg);
+    z-index: 1;
+}
+</style>
